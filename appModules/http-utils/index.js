45dcc61c778b02bc3953c7590acb937457c95512
hynkififor <150060347+hynkififor@users.noreply.github.com>
2024-04-27 23:47:19 +0300

    Загрузила работу

---
+++ appModules/http-utils/index.js
@@ -0,0 +1,9 @@
+const mineTypes = require("./mine-types");
+const staticFile = require("./static-file");
+const parseBody = rwquire("./parse-body");
+
+module.exports = {
+    mineTypes,
+    staticFile,
+    parseBody
+}
